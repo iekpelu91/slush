a0b82affe3ef345d6e1b0c701bcc2a0952a8f218
Imaoye Ekpelu <iekpelu@slu.edu>
2013-09-13 14:50:16 -0500

    Added include commands

--- slush.c
+++ slush.c
@@ -1,2 +1,24 @@
-//Imaoye Ekpelu
+/* Imaoye Ekpelu and Mark Tabor
+ * SLUSH
+ * Operating Systems
+ * September 13, 2013
+ *
+ *
+ *
+ */
 
+
+//for strtok, strcmp
+#include <string.h>
+//for close, dup2, pipe, execvp, fork
+#include <unistd.h>
+//for dup2, pipe
+#include <fcntl.h>
+//for pipe
+#define _GNU_SOURCE
+//for wait
+#include <sys/types.h>
+//for wait
+#include <sys/wait.h>
+//for signal, siginterrupt
+#include <signal.h>
